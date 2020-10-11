5a57c45bf34eab4121d0547943dbb308597a11de
Cyril MORISSE <cmorisse@boxes3.net>
2020-10-11 21:49:51 +0200

    WIP

--- payment_lyra/models/payment.py
+++ payment_lyra/models/payment.py
@@ -14,6 +14,7 @@ import hmac
 import logging
 import math
 import json
+import requests
 from os import path
 
 from pkg_resources import parse_version
@@ -29,13 +30,14 @@ from ..helpers import constants, tools
 from .card import LyraCard
 from .language import LyraLanguage
 
-
 try:
     import urlparse
 except ImportError:
     import urllib.parse as urlparse
 
+
 _logger = logging.getLogger(__name__)
+
 
 # Maps Odoo save_token selection value Lyra 'vads_page_action'
 vads_page_action_MAP = {
@@ -72,7 +74,6 @@ class AcquirerLyra(models.Model):
         providers.append(('lyramulti', _('Lyra Collect - Payment in installments')))
 
     provider = fields.Selection(selection_add=providers)
-
     lyra_site_id = fields.Char(string=_('Shop ID'), help=_('The identifier provided by Lyra Collect.'), default=constants.LYRA_PARAMS.get('SITE_ID'))
     lyra_key_test = fields.Char(string=_('Key in test mode'), help=_('Key provided by Lyra Collect for test mode (available in Lyra Expert Back Office).'), default=constants.LYRA_PARAMS.get('KEY_TEST'), readonly=constants.LYRA_PLUGIN_FEATURES.get('qualif'))
     lyra_key_prod = fields.Char(string=_('Key in production mode'), help=_('Key provided by Lyra Collect (available in Lyra Expert Back Office after enabling production mode).'), default=constants.LYRA_PARAMS.get('KEY_PROD'))
@@ -97,9 +98,20 @@ class AcquirerLyra(models.Model):
     lyra_multi_period = fields.Char(string=_('Period'), help=_('Delay (in days) between payments.'))
     lyra_multi_first = fields.Char(string=_('1st payment'), help=_('Amount of first payment, in percentage of total amount. If empty, all payments will have the same amount.'))
 
+    lyra_rest_api_server_url = fields.Char(
+        "Server URL", 
+        help=_("API REST Server URL as defined in your BackOffice (Settings / Shops / REST API Keys)"))
+    lyra_rest_api_password_test = fields.Char(
+        "API Test Password",
+        help=_("As defined in your BackOffice (Settings / Shops / REST API Keys)")
+    )
+    lyra_rest_api_password_prod = fields.Char(
+        "API Production Password",
+        help=_("As defined in your BackOffice (Settings / Shops / REST API Keys)")
+    )
+
     # Check if it's Odoo 10.
     lyra_odoo10 = True if parse_version(release.version) < parse_version('11') else False
-
     # Compatibility betwen Odoo 13 and previous versions.
     lyra_odoo13 = True if parse_version(release.version) >= parse_version('13') else False
 
@@ -112,7 +124,6 @@ class AcquirerLyra(models.Model):
 
     def _get_feature_support(self):
         """Get advanced feature support by provider.
-
         Each provider should add its technical in the corresponding
         key for the following features:
             * tokenize: support saving payment data in a payment.tokenize
@@ -125,33 +136,26 @@ class AcquirerLyra(models.Model):
     @api.model
     def multi_add(self, filename):
         file = path.join(path.dirname(path.dirname(path.abspath(__file__)))) + filename
-
         if (constants.LYRA_PLUGIN_FEATURES.get('multi') == True):
             convert_xml_import(self._cr, 'payment_lyra', file)
-
         return None
 
     def _get_ctx_mode(self):
         ctx_key = self.state if self.lyra_odoo13 else self.environment
         ctx_value = 'TEST' if ctx_key == 'test' else 'PRODUCTION'
-
         return ctx_value
 
     def _lyra_generate_sign(self, acquirer, values):
         key = self.lyra_key_prod if self._get_ctx_mode() == 'PRODUCTION' else self.lyra_key_test
-
         sign = ''
         for k in sorted(values.keys()):
             if k.startswith('vads_'):
                 sign += values[k] + '+'
-
         sign += key
-
         if self.lyra_sign_algo == 'SHA-1':
             shasign = sha1(sign.encode('utf-8')).hexdigest()
         else:
             shasign = base64.b64encode(hmac.new(key.encode('utf-8'), sign.encode('utf-8'), sha256).digest()).decode('utf-8')
-
         return shasign
 
     def _get_payment_config(self, amount):
@@ -286,6 +290,50 @@ class AcquirerLyra(models.Model):
 
     def lyramulti_get_form_action_url(self):
         return self.lyra_gateway_url
+
+    def _get_lyra_rest_api_url(self):
+        """ Returns Lyra base URL API """
+        return urlparse.urljoin(self.lyra_rest_api_server_url, "api-payment/V4/")
+
+    def _generate_lyra_auth_header(self):
+        """ Generate Lyra REST API auth header according to Shop 'mode' (Production or Test) """
+        if self.state == 'enabled':
+            assert self.lyra_rest_api_password_prod, "REST API Prod password is undefined"
+            rest_api_password = self.lyra_rest_api_password_prod
+        else:
+            rest_api_password = self.lyra_rest_api_password_test
+        auth_string = "{}:{}".format(self.lyra_site_id, rest_api_password,)
+        auth_string_b64 = base64.b64encode(auth_string.encode('utf-8'))
+        return "Basic %s" % auth_string_b64.decode('utf-8')
+
+    def _lyra_API_request(self, url, json_data=None, method='POST'):
+        """ Call Lyra Rest API """
+        self.ensure_one()
+        url = urlparse.urljoin(self._get_lyra_rest_api_url(), url)
+        headers = {
+            'Authorization': self._generate_lyra_auth_header(),
+            'Content-Type': 'application/json', 
+        }
+        resp = requests.request(method, url, json=json_data, headers=headers)
+        if not resp.ok and (400 <= resp.status_code < 500 and resp.json().get('error', {}).get('code')):
+            try:
+                resp.raise_for_status()
+            except HTTPError:
+                _logger.error(resp.text)
+                lyra_error = resp.json().get('error', {}).get('message', '')
+                error_msg = " " + (_("Lyra gave us the following info about the problem: '%s'") % lyra_error)
+                raise ValidationError(error_msg)
+        resp_dict = resp.json()
+        if resp_dict.get('status')=="ERROR":
+            lyra_error = "%s: %s" % (
+                resp_dict.get('answer', {}).get('errorCode', 'errorCode NotFound'),
+                resp_dict.get('answer', {}).get('errorMessage', 'errorMessage NotFound')
+            )
+            _logger.error(lyra_error)
+            lyra_detailed_error = resp_dict.get('answer', {}).get('detailedErrorMessage', 'detailedErrorMessage NotFound')
+            error_msg = " " + (_("Lyra gave us the following info about the problem: '%s'") % lyra_detailed_error)
+            raise ValidationError(error_msg)
+        return 
 
 
 class TransactionLyra(models.Model):
@@ -443,7 +491,94 @@ class TransactionLyra(models.Model):
             })
 
             self.write(values)
-
             return False
+
+    #
+    # API Related Methods
+    #
+    @api.model
+    def _lyra_api_get_tx_from_response(self, resp_dict):
+        transaction_dict = resp_dict['transactions'][0]
+        reference = resp_dict['answer']['orderDetails']['orderId']
+        status = transaction_dict['detailedStatus']
+
+        if not reference or not status:
+            error_msg = 'Lyra Collect : received bad data {}'.format(data)
+            _logger.error(error_msg)
+            raise ValidationError(error_msg)
+
+        tx = self.search([('reference', '=', reference)])
+        if not tx or len(tx) > 1:
+            error_msg = 'Lyra Collect: received data for reference {}'.format(reference)
+            if not tx:
+                error_msg += '; no order found'
+            else:
+                error_msg += '; multiple order found'
+
+            _logger.error(error_msg)
+            raise ValidationError(error_msg)
+        return tx
+
+
+    def _lyra_createPayment_payload(self):
+        """ Generate payload for CreatePayment WS Call (using Token based payment)
+        """
+        assert self.payment_token_id.acquirer_ref, "Missing Required paymentMethodToken"
+        currency_num = tools.find_currency(self.currency_id.name)
+        if currency_num is None:
+            _logger.error('The plugin cannot find a numeric code for the current shop currency {}.'.format(values['currency'].name))
+            # TODO: Check validation error is in the log
+            raise ValidationError(_('The shop currency {} is not supported.').format(values['currency'].name))
+
+        create_payment_payload = {
+            'orderId': self.reference,
+            'contrib': constants.LYRA_PARAMS.get('CMS_IDENTIFIER') + u'_' + constants.LYRA_PARAMS.get('PLUGIN_VERSION') + u'/' + release.version,
+            'currency': currency_num,
+            'amount': int(self.amount * (10 ** self.currency_id.decimal_places)),
+            "formAction": "SILENT",  # Effectue un paiement par alias sans passer par le formulaire embarqué.
+            'paymentMethodToken':self.payment_token_id.acquirer_ref,  # TODO:
+            "strongAuthentication": "DISABLED",  # Permet de demander une authentification sans interaction (frictionless). Le marchand perd la garantie de paiement si la demande est acceptée.
+            "customer": {
+                "reference": self.partner_id.id,  # str[63] Identifiant de l’acheteur chez le marchand.
+                "email": self.partner_email,
+                "billingDetails": {
+                    "legalName": self.partner_name,
+                    "address": self.partner_address,
+                    "zipCode": self.partner_zip,
+                    "phoneNumber": self.partner_phone,
+                    "country": self.partner_country_id.code,
+                    "city": self.partner_city,
+                    "language": self.partner_lang
+                }
+            },
+            "metadata": {
+                "payment_transaction_ref": self.reference
+            },
+            "transactionOptions": {
+                "cardOptions": {
+                    "paymentSource": "OTHER"  # Autre canal de vente. Valeur de sortie retournée pour les paiements réalisés depuis le Back-Office, les paiements par fichier, les paiements récurrents, les paiements de proximité, les remboursements depuis le CMS Shopify.
+                    #"manualValidation": None,  # Configuration par défaut de la boutique retenue (paramétrable dans le Back Office Marchand).
+                    #"captureDelay": None  #  Indique le délai en nombre de jours avant remise en banque. Si null, la valeur par défaut définie dans le Back Office Marchand sera utilisée.
+                }
+            }
+        }
+        _logger.info("_lyra_createPayment generated values:%s", json.dumps(create_payment_payload, indent=4))
+        #res = self.acquirer_id._lyra_request('payment_intents', create_payment_payload)
+        #_logger.info('_lyra_createPayment: response received:\n%s', pprint.pformat(res))
+        #return res
+        return create_payment_payload
+
+    def _lyra_process_createPayment_response(self, resp_dict):
+        """ Process Lyra API createPayement Response and update transaction accordingly. """
+        
+
+
+    def lyra_s2s_do_transaction(self, **kwargs):
+        """ Called to initiate a system to system (s2s) transaction. """
+        self.ensure_one()
+        createPayzen_payload = self._lyra_createPayment_payload()
+        lyra_resp = self.acquirer_id._lyra_API_request('Charge/CreatePayment', createPayzen_payload)
+        self.
+        return lyra_resp
 
 
